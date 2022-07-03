903ec304ba7fb87a353b111136b5653cbb682bde
pidokige02 <pidokige0204@gmail.com>
2022-07-03 14:43:38 +0900

    second commit

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -15,7 +15,7 @@ import { Movie } from './entities/movie.entity';
 @Controller('movies') // it's the base url
 export class MoviesController {
     constructor(private readonly moviesService: MoviesService) {}
-
+    // get http://localhost:3000/movies/
     @Get()
     getAll() {
         return this.moviesService.getAll();
@@ -25,27 +25,32 @@ export class MoviesController {
     search(@Query('year') seachingYear: string) {
         return `We are searching for a movie made after: ${seachingYear}`;
     }
-
-    @Get("/:id")
+    // get http://localhost:3000/movies/1
+    @Get(":id")
     getOne(@Param("id") movieId:string) : Movie {
         return this.moviesService.getOne(movieId);
     }
-
+    // POST: http://localhost:3000/movies/
+    // {
+    //     "title" : "Waiting for me",
+    //     "year" : 2020,
+    //     "genres" : ["action", "mind blown"]
+    // }
     @Post()
     create(@Body() movieData) {
         return this.moviesService.create(movieData);
     }
 
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param("id") movieId:string) {
         return this.moviesService.deleteOne(movieId);
     }
-
+    // http://localhost:3000/movies/1
+    // {
+    //     "year": 2022,
+    // }
     @Patch(':id')
     patch(@Param('id') movieId: string, @Body() updateData) {
-        return {
-        updatedMovie: movieId,
-        ...updateData,
-        };
+        return this.moviesService.update(movieId, updateData);
     }
 }
